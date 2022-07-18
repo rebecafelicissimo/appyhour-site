8b4414094c8fc1ce1f2524e0e2b9f84bb50e7028
appy-hour <81193735+rebecafelicissimo@users.noreply.github.com>
2022-07-18 13:07:56 -0300

    criamos arquivo javascript
    Sem conteúdo ainda

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+/* Quando eu scrollar o mouse para baixo, quero que aconteça o seguinte no cabeçalho: 
+1 - mudar o backgroud para #f6f6f6;
+2 - adicionar o box shadow;
+*/
+
+// selecionar e guardar em uma variável o meu header
+const header = document.querySelector("#header");
+
+console.log(scrollY);
